118cede8cb98c6841d08aca8d3be5afb707550d3
t.jacquemard <t.jacquemard@gda.fr>
2025-06-30 16:30:49 +0200

    add webpack-encore

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+
+import '../scss/app.scss';
