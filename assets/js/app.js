c1f20df8bce92e751e9512f98f341f8fa524bf89
tjacquemard-gda <t.jacquemard@gda.fr>
2025-07-04 16:22:25 +0200

    Revert "Feature/revamp check"

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-import '../scss/app.scss';
-
-import hideUnless from './utils/_hideUnless.js';
-
-window.addEventListener('load', function () {
-    hideUnless();
-});
